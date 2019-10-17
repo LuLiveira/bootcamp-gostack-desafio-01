49327eab1625beefc8da5a94fa29c64eaa491def
Lucas Oliveira <lcsd.lucas@gmail.com>
2019-10-17 15:17:55 -0300

    Ajustando middleware

--- index.js
+++ index.js
@@ -3,24 +3,25 @@ const ProjectController = require('./src/controllers/ProjectController')
 
 const app = express();
 
-var cont = 0;
+let cont = 0;
 
 function contador(request, response, next){
-    ++cont;
-    console.log(cont);
+    console.log(++cont);    
     return next();
 }
 
+app.use(contador);
+
 app.use(express.json());
 
-app.get('/projects', contador, ProjectController.index);
+app.get('/projects', ProjectController.index);
 
-app.post('/projects', contador, ProjectController.store);
+app.post('/projects', ProjectController.store);
 
-app.post('/projects/:id/tasks', contador, ProjectController.tasks);
+app.post('/projects/:id/tasks',  ProjectController.tasks);
 
-app.put('/projects/:id', contador, ProjectController.update);
+app.put('/projects/:id',  ProjectController.update);
 
-app.delete('/projects/:id', contador, ProjectController.remove);
+app.delete('/projects/:id',  ProjectController.remove);
 
 app.listen(3000);
